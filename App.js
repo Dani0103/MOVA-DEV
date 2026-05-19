9914d7dcf8852f02063da57209e986ac27106ecb
Daniel Felipe Ruiz Tovar <fepiperuiz11@gmail.com>
2026-05-18 23:18:01 -0500

    Revert "feat: banner de descarga de app en navegador móvil"
    This reverts commit 04cdec903db5564b61ad234b68910cda55378713.

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { AccountProvider } from "./src/context/AccountContext";
 import { CategoryProvider } from "./src/context/CategoryContext";
 import { ThemeProvider } from "./src/context/ThemeContext";
 import AppNavigator from "./src/navigation/AppNavigator";
-import AppDownloadBanner from "./src/components/ui/AppDownloadBanner";
 
 export default function App() {
   return (
@@ -12,8 +11,6 @@ export default function App() {
         <AccountProvider>
           <CategoryProvider>
             <AppNavigator />
-            {/* Banner de descarga — solo visible en web + navegador móvil */}
-            <AppDownloadBanner />
           </CategoryProvider>
         </AccountProvider>
       </AuthProvider>
